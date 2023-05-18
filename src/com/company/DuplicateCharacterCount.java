82ccb8185a23c973a6e95185a72932496feddf7a
ShivajiBajpai98 <84443265+ShivajiBajpai98@users.noreply.github.com>
2023-05-18 18:04:35 +0530

    Update DuplicateCharacterCount.java

--- src/com/company/DuplicateCharacterCount.java
+++ src/com/company/DuplicateCharacterCount.java
@@ -1,46 +1,28 @@
-package com.company;
-
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class DuplicateCharacterCount {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter a word: ");
-        String word =scanner.nextLine();
-        int count;
-        //Converts given string into character array
-        char[] character = word.toCharArray();
-        System.out.println("Duplicate characters in the given string: ");
-        //Counts each character present in the string
-        for(int index = 0; index <character.length; index++)
-        {
-            count = 1;
-            for(int range = index+1; range <character.length; range++)
-            {
-                if(character[index] == character[range] && character[index] != ' ')
-                {
-                    count++;
-                    //Set string[j] to 0 to avoid printing visited character
-                    character[range] = '0';
-                }
-            }
-            //A character is considered as duplicate if count is greater than 1
-            if(count > 1 && character[index] != '0')
-                System.out.println(character[index]);
-        }
-        int wordCount = 0;
+        String word = scanner.nextLine().toLowerCase();
+        scanner.close();
 
-        for (char letter = 'a'; letter <= 'z'; letter++) {
-            for (int scope = 0; scope < word.length(); scope++) {
-                if (word.charAt(scope) == letter) {
-                    wordCount++;
-                }
+        Map<Character, Integer> charCountMap = new HashMap<>();
+
+        // Count each character in the string
+        for (char c : word.toCharArray()) {
+            if (Character.isLetter(c)) {
+                charCountMap.put(c, charCountMap.getOrDefault(c, 0) + 1);
             }
-            if (wordCount > 0) {
-                System.out.println(letter + " repated = " + wordCount+" times");
-                wordCount = 0;
+        }
+
+        System.out.println("Duplicate characters in the given string: ");
+        for (Map.Entry<Character, Integer> entry : charCountMap.entrySet()) {
+            if (entry.getValue() > 1) {
+                System.out.println(entry.getKey() + " repeated " + entry.getValue() + " times");
             }
         }
     }
